c5f31f164b9b0adb20152ad474a86f2201d3f5d3
PyricRL <ljcosa@icloud.com>
2025-04-25 08:57:58 -0500

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,58 @@
+#include "SDL3/SDL.h"
+
+static SDL_Window* window;
+static SDL_Renderer* renderer;
+
+/**
+ * For setting up CMake:
+ * git clone https://github.com/libsdl-org/SDL.git include/SDL
+ * create main.c/cpp file
+ * create CMakeLists.txt file
+ * mkdir build
+ * cd build
+ * cmake ..
+ * cmake --build .
+ * ./<exe-name>
+ */
+
+/**
+ * For buttons at bottom:
+ * need cmake tools extension
+ * ctrl + shift + p
+ * >CMake: Configure
+ * >CMake: Build
+ * >CMake: Run Without Debugging
+ * >CMake: Debug
+ */
+
+void update(SDL_Renderer* renderer) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+}
+
+int main(int argc, char* argv[]) {
+	if (!SDL_Init(SDL_INIT_VIDEO)) {
+		SDL_Log("SDL_Init Error: %s", SDL_GetError());
+		return 1;
+	}
+
+	if (!SDL_CreateWindowAndRenderer("Hello SDL!", 800, 600, 0, &window, &renderer)) {
+		SDL_Log("SDL_CreateWindowAndRenderer Error: %s", SDL_GetError());
+		return 1;
+	}
+
+    bool running = true;
+
+	SDL_Event event;
+
+	while (running) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_EVENT_QUIT) {
+				running = false;
+			}
+		}
+
+        update(renderer);
+    }
+}
